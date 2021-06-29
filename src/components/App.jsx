cb7f0b7f6493aaa70b13cdc181c212486c2c9eba
Patrick Sawyer <patrick_sawyer@hotmail.co.uk>
2021-06-29 10:36:41 +0100

    Get posts working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,21 @@
+import React, {useEffect, useState} from 'react';
+
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
+  const getPosts = () => {
+    fetch('/api/posts')
+    .then(response => response.json())
+    .then(data => {
+      setPosts(data);
+    });
+  }
+
   return <div>{/* Complete the exercise here. */}</div>;
 }
 
